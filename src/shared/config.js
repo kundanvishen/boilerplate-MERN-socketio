e8d9d04ee9991eaecdb405e584d73b113a968773
Kundan Singh <kundan.singh979@gmail.com>
2018-09-22 23:02:54 +0530

    [feat]: webpack config - part1

--- src/shared/config.js
+++ src/shared/config.js
@@ -3,3 +3,7 @@
 export const WEB_PORT = process.env.PORT || 8000;
 export const STATIC_PATH = '/static';
 export const APP_NAME = 'My New JS App';
+
+export const WDS_PORT = 7000;
+export const APP_CONTAINER_CLASS = 'js-stack';
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`;
